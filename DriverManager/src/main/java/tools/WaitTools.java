0a02f9d64bf0937ccd9809a2af15e6f0341d6c5a
valeriazz <valeriazlk21@gmail.com>
2024-02-27 19:58:13 +0300

    Initial commit

---
+++ DriverManager/src/main/java/tools/WaitTools.java
@@ -0,0 +1,24 @@
+package tools;
+
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class WaitTools {
+    private WebDriver driver;
+
+    public WaitTools(WebDriver driver) {
+        this.driver = driver;
+    }
+    public boolean waitForCondition(ExpectedCondition condition) {
+        try {
+            new WebDriverWait(driver, Duration.ofSeconds(10)).until(condition);
+            return true;
+        } catch (TimeoutException ignore) {
+            return false;
+        }
+    }
+}
